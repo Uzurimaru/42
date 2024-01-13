df503fbe9bc3fa701b9a40bc680ec7c319b10e05
Romain Wintgens <rwintgen@3B3.42angouleme.fr>
2024-01-13 16:49:19 +0100

    push_swap

--- push_swap/sources/push_swap.c
+++ push_swap/sources/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: rwintgen <rwintgen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 14:24:20 by rwintgen          #+#    #+#             */
-/*   Updated: 2024/01/11 22:18:00 by rwintgen         ###   ########.fr       */
+/*   Updated: 2024/01/13 16:47:56 by rwintgen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,110 @@
 #include <stdio.h>
 
 // TODO modif libft makefile to add ft_printf
+// TODO check why segfault when giving number > INT_MAX as argv
+
+////////// UTIL //////////
+
+t_node	*ft_get_max(t_node *stack)
+{
+	int		max;
+	t_node	*max_node;
+
+	max = INT_MIN;
+	max_node = NULL;
+	while (stack)
+	{
+		if (stack->nb > max)
+		{
+			max = stack->nb;
+			max_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (max_node);
+}
+
+t_node	*ft_get_min(t_node *stack)
+{
+	int		min;
+	t_node	*min_node;
+
+	min = INT_MAX;
+	min_node = NULL;
+	while (stack)
+	{
+		if (stack->nb > min)
+		{
+			min = stack->nb;
+			min_node = stack;
+		}
+		stack = stack->next;
+	}
+	return (min_node);
+}
+
+int	ft_stack_len(t_node *stack)
+{
+	int	i;
+
+	i = 0;
+	while (stack)
+	{
+		stack = stack->next;
+		i++;
+	}
+	return (i);
+}
+
+t_node	*ft_get_last_node(t_node *stack)
+{
+	if (!stack)
+		return (NULL);
+	while (stack->next)
+		stack = stack->next;
+	return (stack);
+}
+
+int	ft_stack_sorted(t_node *stack)
+{
+	while (stack->next)
+	{
+		if (stack->nb >= stack->next->nb)
+			return (0);
+		stack = stack->next;
+	}
+	return (1);
+}
+
+void	ft_print_stack(t_node *stack_a, t_node *stack_b)
+{
+	int	index;
+	int	max;
+	int	len_a;
+	int	len_b;
+
+	len_a = ft_stack_len(stack_a);
+	len_b = ft_stack_len(stack_b);
+	max = len_a;
+	if (len_a < len_b)
+		max = len_b;
+	index = 0;
+	while (index < max)
+	{
+		printf("%d", stack_a->nb);
+		if (!(len_b < (max - index)))
+		{
+			printf("\t%d\n", stack_b->nb);
+			stack_b = stack_b->next;
+			len_b--;
+		}
+		else
+			printf("\n");
+		stack_a = stack_a->next;
+		index++;
+	}
+	printf("_\t_\na\tb\n\n");
+}
 
 ////////// CMDS //////////
 
@@ -27,6 +131,8 @@ void	ft_swap(t_node **top)
 
 	if (!(*top) || !((*top)->next))
 		return ;
+	sec = (*top)->next;
+	thr = sec->next;
 	(*top)->prev = sec;
 	(*top)->next = thr;
 	sec->prev = NULL;
@@ -68,78 +174,80 @@ void	ft_push(t_node **src, t_node **dst)
 	*dst = *src;
 	*src = src_sec;
 }
-
-////////// UTIL //////////
-
-t_node	*ft_get_max(t_node *stack)
+void 	pa(t_node **stack_a, t_node **stack_b)
 {
-	int		max;
-	t_node	*max_node;
-
-	max = INT_MIN;
-	max_node = NULL;
-	while (stack)
-	{
-		if (stack->nb > max)
-		{
-			max = stack->nb;
-			max_node = stack;
-		}
-		stack = stack->next;
-	}
-	return (max_node);
+	ft_push(stack_a, stack_b);
 }
 
-t_node	*ft_get_min(t_node *stack)
+void 	pb(t_node **stack_b, t_node **stack_a)
 {
-	int 	min;
-	t_node	*min_node;
-
-	min = INT_MAX;
-	min_node = NULL;
-	while (stack)
-	{
-		if (stack->nb > min)
-		{
-			min = stack->nb;
-			min_node = stack;
-		}
-		stack = stack->next;
-	}
-	return (min_node);
+	ft_push(stack_b, stack_a);
 }
 
-int	ft_stack_len(t_node *stack)
-{
-	int	i;
+////////// ROTATE
 
-	i = 0;
-	while (stack)
-	{
-		stack = stack->next;
-		i++;
-	}
-	return (i);
+void	ft_rotate(t_node **top)
+{
+	t_node	*sec;
+	t_node	*last;
+
+	if (!(*top) || !(*top)->next)
+		return ;
+	sec = (*top)->next;
+	last = ft_get_last_node(*top);
+	(*top)->next = NULL;
+	(*top)->prev = last;
+	last->next = *top;
+	sec->prev = NULL;
+	*top = sec;
 }
 
-t_node	*ft_get_last_node(t_node *stack)
+void	ft_rev_rotate(t_node **top)
 {
-	if (!stack)
-		return (NULL);
-	while (stack->next)
-		stack = stack->next;
-	return (stack);
+	t_node	*last;
+	t_node	*b_last;
+
+	if (!(*top) || !(*top)->next)
+		return ;
+	last = ft_get_last_node(*top);
+	b_last = last->prev;
+	(*top)->prev = last;
+	last->prev = NULL;
+	last->next = *top;
+	b_last->next = NULL;
+	*top = last;
 }
 
-int	ft_is_stack_sorted(t_node *stack)
+void	ra(t_node **stack_a)
 {
-	while (stack->next)
-	{
-		if (stack->nb >= stack->next->nb)
-			return (1);
-		stack = stack->next;
-	}
-	return (0);
+	ft_rotate(stack_a);
+}
+
+void	rb(t_node **stack_b)
+{
+	ft_rotate(stack_b);
+}
+
+void	rr(t_node **stack_a, t_node **stack_b)
+{
+	ft_rotate(stack_a);
+	ft_rotate(stack_b);
+}
+
+void	rra(t_node **stack_a)
+{
+	ft_rev_rotate(stack_a);
+}
+
+void	rrb(t_node **stack_b)
+{
+	ft_rev_rotate(stack_b);
+}
+
+void	rrr(t_node **stack_a, t_node **stack_b)
+{
+	ft_rev_rotate(stack_a);
+	ft_rev_rotate(stack_b);
 }
 
 ////////// ERRS //////////
@@ -234,7 +342,6 @@ void	ft_lst_append(t_node **stack, int nb)
 		last_node->next = new_node;
 		new_node->prev = last_node;
 	}
-	printf("%d\n", new_node->nb);
 }
 
 void	ft_init_stack_a(t_node **stack_a, char **argv)
@@ -255,6 +362,8 @@ void	ft_init_stack_a(t_node **stack_a, char **argv)
 		ft_lst_append(stack_a, nb);
 		i++;
 	}
+	printf("\n===============\n\nstack init:\n\n");
+	ft_print_stack(*stack_a, NULL);
 }
 
 ////////// ALGO //////////
@@ -271,11 +380,21 @@ void	ft_easy_sort(t_node **stack_a, int stack_len)
 		big = ft_get_max(*stack_a);
 		if (big == *(stack_a))
 			ra(stack_a);
-		else if (big == *(stack_a)->next)
+		else if (big == (*stack_a)->next)
 			rra(stack_a);
-		if (*(stack_a)->nb > *(stack_a)->next->nb)
+		if ((*stack_a)->nb > (*stack_a)->next->nb)
 			sa(stack_a);
 	}
+	printf("final result:\n\n");
+	ft_print_stack(*stack_a, NULL);
+}
+
+void	ft_hard_sort(t_node **stack_a, t_node **stack_b)
+{
+	(void) stack_a;
+	(void) stack_b;
+
+	printf("missing 3+ numbers algo.\nget to work!\n\n");
 }
 
 ////////// MAIN //////////
@@ -283,10 +402,10 @@ void	ft_easy_sort(t_node **stack_a, int stack_len)
 int	main(int argc, char **argv)
 {
 	t_node	*stack_a;
-	// t_node	*stack_b;
+	t_node	*stack_b;
 
 	stack_a = NULL;
-	// stack_b = NULL;
+	stack_b = NULL;
 
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		ft_err_free(1, &stack_a);
@@ -298,13 +417,18 @@ int	main(int argc, char **argv)
 	else
 		ft_init_stack_a(&stack_a, argv + 1);
 
-	if (!ft_stack_sorted(a))
+	if (!ft_stack_sorted(stack_a))
 	{
+		printf("sorting stack...\n\n");
 		if (ft_stack_len(stack_a) < 4)
-			ft_easy_sort(stack_a, ft_stack_len(stack_a));
+			ft_easy_sort(&stack_a, ft_stack_len(stack_a));
 		else
-			ft_hard_sort(stack_a, stack_b);
+			ft_hard_sort(&stack_a, &stack_b);
 	}
+	if (ft_stack_sorted(stack_a))
+		printf("stack is sorted.\n\n");
+	else
+		printf("stack could not be sorted.\n\n");
 	ft_free_stack(&stack_a);
 	ft_free_stack(&stack_b);
 	return (0);
